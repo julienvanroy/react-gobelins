e52e5265eb102624614ae99695a476bef21de836
Juju <juliette.sageaubriot@gmail.com>
2019-11-17 19:52:00 +0100

    feat: update component home
    feat: add editor config

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { format } from 'path';
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
-};
+class UserForm extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {value: ''}
+    }
 
-export default Home;
+
+
+    render() {
+        return(
+            <form> 
+            <div class="form-group">
+                <label for="exampleUsername">Username</label>
+                <input type="username" class="form-control" id="exampleUsername" aria-describedby="usernameHelp" placeholder="Enter username"></input>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password"></input>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        )
+    }
+
+}
+
+export default UserForm;
